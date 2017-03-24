0e818e3f6d977a07b43db03fa9735de5e8115612
Haoliang Gao <sakura9515@gmail.com>
2017-03-24 10:49:03 +0800

    test: fix test on node4 (#33)

---
+++ .autod.conf.js
@@ -0,0 +1,21 @@
+'use strict'
+
+module.exports = {
+  write: true,
+  prefix: '^',
+  devprefix: '^',
+  exclude: [
+  ],
+  devdep: [
+    'autod',
+    'egg-bin',
+    'egg-ci',
+    'eslint',
+    'eslint-config-egg',
+  ],
+  keep: [
+  ],
+  semver: [
+    'egg-bin@1',
+  ],
+};
